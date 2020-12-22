1946831060665c6a706d166fa620363c75d040ed
Jeff <35370144+Hardy0529@users.noreply.github.com>
2020-12-22 13:39:43 +0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+
+	// logo change when scroll
+	$(window).on('scroll',function(){
+		var scrollDistance = $(window).scrollTop();
+		var $header = $(".js-header");
+		if(scrollDistance > 80){
+			$header.addClass("header--scrolling");
+		}
+		else{
+			$header.removeClass("header--scrolling")
+		}
+	})
+
+
+});
