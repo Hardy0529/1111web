ca92aeab71c4d5a89e0a00f1ba0840bb4544b515
Jeff <35370144+Hardy0529@users.noreply.github.com>
2020-12-28 14:57:27 +0800

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -129,10 +129,24 @@ $(document).ready(function() {
     });
 
   
-
-    document.documentElement.addEventListener('touchmove', function (event) { 
-        event.preventDefault(); 
-    }, false); 
-
+    // 縮放 IOS
+    window.onload=function () {
+        document.addEventListener('touchstart',function (event) {
+        if(event.touches.length>1){
+        event.preventDefault();
+        }
+        });
+        var lastTouchEnd=0;
+        document.addEventListener('touchend',function (event) {
+        var now=(new Date()).getTime();
+        if(now-lastTouchEnd<=300){
+        event.preventDefault();
+        }
+        lastTouchEnd=now;
+        },false);
+        document.addEventListener('gesturestart', function (event) {
+        event.preventDefault();
+        });
+        }
 
 });
