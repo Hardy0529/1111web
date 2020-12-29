932c7816446b172096a6853acfa4f566f19cd5b6
Jeff <35370144+Hardy0529@users.noreply.github.com>
2020-12-29 08:47:21 +0800

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -20,27 +20,7 @@ $(document).ready(function() {
     event.preventDefault();
     });
     }
-    move:function(event){
-       // 縮放 IOS
-    window.onload=function () {
-        document.addEventListener('touchstart',function (event) {
-        if(event.touches.length>1){
-        event.preventDefault();
-        }
-        });
-        var lastTouchEnd=0;
-        document.addEventListener('touchend',function (event) {
-        var now=(new Date()).getTime();
-        if(now-lastTouchEnd<=300){
-        event.preventDefault();
-        }
-        lastTouchEnd=now;
-        },false);
-        document.addEventListener('gesturestart', function (event) {
-        event.preventDefault();
-        });
-        }
-        }
+
 
 
 
