93fec3504fe5112abe4ace854663f0a36a98c408
Jeff <35370144+Hardy0529@users.noreply.github.com>
2021-01-08 15:14:32 +0800

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -98,37 +98,37 @@ $(document).ready(function() {
     // );
 
 
-    $(window).scroll(function(){
-        document.addEventListener('touchstart', function(event) {
-            if (event.touches.length > 1) {
-                event.preventDefault();
-            }
-        });
-        var lastTouchEnd = 0;
-        document.addEventListener('touchend', function(event) {
-            var now = (new Date()).getTime();
-            if (now - lastTouchEnd <= 300) {
-                event.preventDefault();
-            }
-            lastTouchEnd = now;
-        }, false);
-        document.addEventListener('gesturestart', function(event) {
-            event.preventDefault();
-        });
+    // $(window).scroll(function(){
+    //     document.addEventListener('touchstart', function(event) {
+    //         if (event.touches.length > 1) {
+    //             event.preventDefault();
+    //         }
+    //     });
+    //     var lastTouchEnd = 0;
+    //     document.addEventListener('touchend', function(event) {
+    //         var now = (new Date()).getTime();
+    //         if (now - lastTouchEnd <= 300) {
+    //             event.preventDefault();
+    //         }
+    //         lastTouchEnd = now;
+    //     }, false);
+    //     document.addEventListener('gesturestart', function(event) {
+    //         event.preventDefault();
+    //     });
        
 
-        window.addEventListener(
-            "touchmove",
-            function(event) {
-                if(event.scale !== 1) {
-                    event.preventDefault();
-                }
-            }, {
-                passive: false
-            }
-        );
+    //     window.addEventListener(
+    //         "touchmove",
+    //         function(event) {
+    //             if(event.scale !== 1) {
+    //                 event.preventDefault();
+    //             }
+    //         }, {
+    //             passive: false
+    //         }
+    //     );
         
-    })
+    // })
 
 
   
