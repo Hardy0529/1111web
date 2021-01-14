8aa36f2694acc10817d278595390e875ccf1f22b
Jeff <35370144+Hardy0529@users.noreply.github.com>
2021-01-14 16:21:58 +0800

    Add files via upload

---
+++ js/common/loading.js
@@ -0,0 +1,46 @@
+// Loading
+
+
+
+
+
+
+loading ='<div class="pageLoading loading-no_scroll">\
+            <div class="pageLoading_logo">\
+                <div class="loading_logo_people">\
+                    <img src="assets/logo_circle_Img.png" alt="">\
+                </div>\
+                <div class="loading-Outer_layer" >\
+                    <span>\
+                        <img src="./assets/img4-1.png" alt="">\
+                    </span>\
+                    <span>\
+                        <img src="./assets/img4-1.png" alt="">\
+                    </span>\
+                    <span>\
+                        <img src="./assets/img4-1.png" alt="">\
+                    </span>\
+                    <span>\
+                        <img src="./assets/img4-1.png" alt="">\
+                    </span>\
+                </div>\
+            </div>\
+            <div class="loading-Text_Img">\
+                <img src="./assets/loading.png" alt="">\
+                <div class="loading-Text">\
+                    <span>點</span>\
+                    <span>亮</span>\
+                    <span>你</span>\
+                    <span>的</span>\
+                    <span>職</span>\
+                    <span>場</span>\
+                    <span>光</span>\
+                    <span>明</span>\
+                    <span>燈</span>\
+                </div>\
+            </div>\
+            <div class="loading">\
+            <div class="bar"></div>\
+            </div>\
+        </div>';
+document.write(loading);
